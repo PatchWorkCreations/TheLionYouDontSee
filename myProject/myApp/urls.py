9e484a813dc4affe5d5868f1c5008a7663933113
Julia <juliavictorio16@Gmail.com>
2024-10-26 12:05:54 +0800

    updated

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path('subscribe/', views.subscribe, name='subscribe'),
+    path('contact/', views.contact, name='contact'),
+    
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 def index(request):
     context={}
     return render(request, 'myApp/index.html', context)
+
+
+
+def subscribe(request):
+    if request.method == "POST":
+        full_name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        # Handle the subscription logic here
+        return HttpResponse("Thank you for subscribing!")
+    return redirect('index')  # Change 'index' to the name of your home page view if different
+
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        
+        # Here you can add functionality to send an email or save the contact message
+        return HttpResponse("Thank you for getting in touch!")
+    
+    # Redirect or render a page if necessary for GET requests
+    return redirect('index')  # Change 'index' to the name of your home page view if different
